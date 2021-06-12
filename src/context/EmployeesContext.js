fdb9a4a879426b1bae776ef1590ee33895459037
SabinaAbbasova <sabina.abbasova2909@gmail.com>
2021-06-12 16:25:33 +0400

    Update EmployeesContext.js

--- src/context/EmployeesContext.js
+++ src/context/EmployeesContext.js
@@ -151,9 +151,9 @@ const EmployeesContextProvider = (props) => {
 
     // PAGINATION
     const paginate = useCallback((pageNumber) => {
-        const indexOfLastBook = pageNumber * employeesPerPage;
-        const indexOfFirstBook = indexOfLastBook - employeesPerPage;
-        const currentEmployees = employees.slice(indexOfFirstBook, indexOfLastBook);
+        const indexOfLastEmployee = pageNumber * employeesPerPage;
+        const indexOfFirstEmployee = indexOfLastEmployee - employeesPerPage;
+        const currentEmployees = employees.slice(indexOfFirstEmployee, indexOfLastEmployee);
         setCurrentEmployees(currentEmployees)
         setCurrentPage(pageNumber)
     }, [employeesPerPage, employees])
